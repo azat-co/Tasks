4968a14ad2eda6b8d81c79fc837e2fd4df5a7a66
Azat Mardanov <azat.mardanov@gmail.com>
2012-06-15 18:59:28 -0700

    datetime formated

--- app/controllers/multi_task_controller.rb
+++ app/controllers/multi_task_controller.rb
@@ -13,14 +13,15 @@ class MultiTaskController < ApplicationController
     else
       task_type_id=params["task_type_id"] #from hidden field
       puts "****************"+params["due_at"]
-      puts "****************"+Date.strptime(params["due_at"], "%m/%d/%Y %T").to_date.to_s,
+      #DateTime.strptime("06/29/2012 10:00:00", "%m/%d/%Y %H:%M:%S").to_date
+      puts "****************"+DateTime.strptime(params["due_at"], "%m/%d/%Y %H:%M:%S").to_date.to_s,
       task=Task.create({:task_type_id=>task_type_id, :title=>params["task_title"], :due_at=>params["due_at"]      })
       # puts "**************"+strtotime(params["due_at"])
       # puts "****************"+params["task"].to_s
       # puts "****************"+params["due_at"].to_s
       fields=Field.find_all_by_task_type_id(task_type_id)
       fields.each do |f|
-        value=Value.create ({
+        value=Value.create({
           :task_id=>task.id,
           :field_id=>f.id,
           :value=>params[f.name]
